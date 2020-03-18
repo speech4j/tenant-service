3c881fed5bd1904d8ac519457acf9c97bdf02546
MarrMika <slobodjanmarija1@gmail.com>
2020-03-18 12:37:40 +0200

    Add an exception handler

---
+++ src/main/java/com/speech4j/tenantservice/dto/handler/ResponseMessageDto.java
@@ -0,0 +1,12 @@
+package com.speech4j.tenantservice.dto.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseMessageDto {
+    private String message;
+}

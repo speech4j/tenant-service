52a384e2c41d97f2518d4b42721de7aed558e71d
MarrMika <slobodjanmarija1@gmail.com>
2020-03-20 13:55:39 +0200

    Add shared postgresql container

---
+++ src/test/java/com/speech4j/tenantservice/AbstractContainerBaseTest.java
@@ -0,0 +1,25 @@
+package com.speech4j.tenantservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@SpringBootTest(classes = TenantServiceApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class AbstractContainerBaseTest {
+    static final PostgreSQLContainer postgreSQLContainer;
+
+
+    static {
+        postgreSQLContainer = new PostgreSQLContainer()
+                .withPassword("password")
+                .withUsername("postgres")
+                .withDatabaseName("tenant_db");
+        postgreSQLContainer.start();
+
+        System.setProperty("spring.datasource.url", postgreSQLContainer.getJdbcUrl());
+        System.setProperty("spring.datasource.password", postgreSQLContainer.getPassword());
+        System.setProperty("spring.datasource.username", postgreSQLContainer.getUsername());
+    }
+}

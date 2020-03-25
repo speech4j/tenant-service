1f83434c5c121a60f78409bd9098cfd357ecdbd4
MarrMika <slobodjanmarija1@gmail.com>
2020-03-25 17:55:15 +0200

    Add application_test.yaml file

--- src/test/java/com/speech4j/tenantservice/controller/AbstractContainerBaseTest.java
+++ src/test/java/com/speech4j/tenantservice/controller/AbstractContainerBaseTest.java
@@ -1,30 +1,34 @@
 package com.speech4j.tenantservice.controller;
 
-import com.speech4j.tenantservice.handler.GlobalExceptionHandler;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 import static org.junit.Assert.assertTrue;
 
+@TestPropertySource("classpath:application_test.yaml")
 public class AbstractContainerBaseTest {
-    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractContainerBaseTest.class);
     static final PostgreSQLContainer postgreSQLContainer;
+
+    @Value("${TEST_POSTGRES_PASSWORD}")
+    private String password;
+    @Value("${TEST_POSTGRES_USERNAME}")
+    private String username;
+    @Value("${TEST_POSTGRES_DB}")
+    private String database;
 
     static {
         postgreSQLContainer = new PostgreSQLContainer()
-                .withPassword("password")
+                .withPassword("postgres")
                 .withUsername("postgres")
                 .withDatabaseName("tenant_db");
         postgreSQLContainer.start();
 
         System.setProperty("spring.datasource.url", postgreSQLContainer.getJdbcUrl());
-        System.setProperty("spring.datasource.password", postgreSQLContainer.getPassword());
-        System.setProperty("spring.datasource.username", postgreSQLContainer.getUsername());
-
-        LOGGER.info("URL:" + System.getProperty("spring.datasource.url"));
     }
 
     @Test

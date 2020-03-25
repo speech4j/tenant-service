afb4d6b5d454a94272bb4343fe112eb4c3b4b641
MarrMika <slobodjanmarija1@gmail.com>
2020-03-25 12:57:17 +0200

    Change .property file to .yaml

--- src/test/java/com/speech4j/tenantservice/controller/AbstractContainerBaseTest.java
+++ src/test/java/com/speech4j/tenantservice/controller/AbstractContainerBaseTest.java
@@ -2,12 +2,10 @@ package com.speech4j.tenantservice.controller;
 
 import com.speech4j.tenantservice.TenantServiceApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest(classes = TenantServiceApplication.class,
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
 public class AbstractContainerBaseTest extends PostgreSQLContainer<AbstractContainerBaseTest> {
     static final PostgreSQLContainer postgreSQLContainer;
 
@@ -20,7 +18,7 @@ public class AbstractContainerBaseTest extends PostgreSQLContainer<AbstractConta
 
     @Override
     public void start(){
-        postgreSQLContainer.start();
+        super.start();
         System.setProperty("spring.datasource.url", postgreSQLContainer.getJdbcUrl());
         System.setProperty("spring.datasource.password", postgreSQLContainer.getPassword());
         System.setProperty("spring.datasource.username", postgreSQLContainer.getUsername());

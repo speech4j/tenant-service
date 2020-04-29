0a1775c84863548cdb0dedd2bcfaca9d1d0d3a07
MarrMika <slobodjanmarija1@gmail.com>
2020-04-29 18:20:36 +0300

    Ignore path patterns related with tenant controller

--- src/main/java/org/speech4j/tenantservice/config/multitenancy/InterceptorConfig.java
+++ src/main/java/org/speech4j/tenantservice/config/multitenancy/InterceptorConfig.java
@@ -14,6 +14,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(tenantInterceptor).excludePathPatterns("/tenants", "/tenants/**");
+        registry.addInterceptor(tenantInterceptor)
+                .excludePathPatterns("/tenants", "/tenants/{id}");
     }
 }

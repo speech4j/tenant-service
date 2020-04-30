d07b02db0d788b9922206944b00052ea1692bd0b
MarrMika <slobodjanmarija1@gmail.com>
2020-04-30 11:29:11 +0300

    Use built-in formatting constructor

--- src/main/java/org/speech4j/tenantservice/config/multitenancy/TenantContext.java
+++ src/main/java/org/speech4j/tenantservice/config/multitenancy/TenantContext.java
@@ -10,7 +10,7 @@ public class TenantContext {
     private static ThreadLocal<String> currentTenant = new ThreadLocal<>();
 
     public static void setCurrentTenant(String tenant) {
-        logger.debug("Setting tenant to " + tenant);
+        logger.debug("Setting tenant to {}", tenant);
         currentTenant.set(tenant);
     }
 

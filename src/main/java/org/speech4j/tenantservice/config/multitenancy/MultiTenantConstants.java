77c7d4513a017c91af687b35702a4f01e3295480
MarrMika <slobodjanmarija1@gmail.com>
2020-04-30 11:55:13 +0300

    Move constans to a final class

--- src/main/java/org/speech4j/tenantservice/config/multitenancy/MultiTenantConstants.java
+++ src/main/java/org/speech4j/tenantservice/config/multitenancy/MultiTenantConstants.java
@@ -1,6 +1,5 @@
 package org.speech4j.tenantservice.config.multitenancy;
 
-public interface MultiTenantConstants {
-
-    String DEFAULT_TENANT_ID = "metadata";
+public final class  MultiTenantConstants{
+    public static String DEFAULT_TENANT_ID = "metadata";
 }

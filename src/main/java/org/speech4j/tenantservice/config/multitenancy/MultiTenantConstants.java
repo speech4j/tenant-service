3fe2d27d65abba293461a8d935ba3d4c8e85ae32
MarrMika <slobodjanmarija1@gmail.com>
2020-05-05 09:51:52 +0300

    Ensure that string concatenation is required and safe for this SQL query. ConnectionProvider class.

--- src/main/java/org/speech4j/tenantservice/config/multitenancy/MultiTenantConstants.java
+++ src/main/java/org/speech4j/tenantservice/config/multitenancy/MultiTenantConstants.java
@@ -2,6 +2,7 @@ package org.speech4j.tenantservice.config.multitenancy;
 
 public final class  MultiTenantConstants{
     public static final String DEFAULT_TENANT_ID = "speech4j";
+    public static final String SQL_CREATE_SCHEMA = "CREATE SCHEMA IF NOT EXISTS %s";
 
     private MultiTenantConstants(){}
 }

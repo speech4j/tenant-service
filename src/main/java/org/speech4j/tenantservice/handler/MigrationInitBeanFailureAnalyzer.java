012e03ce29fc81fa13ea27e8941a980dc79659ad
MarrMika <slobodjanmarija1@gmail.com>
2020-05-06 13:55:43 +0300

    Implement a custom failure analyzer.

---
+++ src/main/java/org/speech4j/tenantservice/handler/MigrationInitBeanFailureAnalyzer.java
@@ -0,0 +1,23 @@
+package org.speech4j.tenantservice.handler;
+
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.boot.diagnostics.AbstractFailureAnalyzer;
+import org.springframework.boot.diagnostics.FailureAnalysis;
+
+public class MigrationInitBeanFailureAnalyzer extends AbstractFailureAnalyzer<BeanCreationException> {
+    @Override
+    protected FailureAnalysis analyze(Throwable rootFailure, BeanCreationException cause) {
+        return new FailureAnalysis(getDescription(cause), getAction(cause), cause);
+    }
+
+    private String getDescription(BeanCreationException ex) {
+        return String.format("The bean [ %s ] could not be injected because of cause: %s",
+                ex.getBeanName(),
+                ex.getCause());
+    }
+
+    private String getAction(BeanCreationException ex) {
+        return String.format("Consider creating a bean with name [ %s ] as a root problem.",
+                ex.getBeanName());
+    }
+}

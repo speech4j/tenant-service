46f5c5a5ea82642298fe1a9fca195520c74da7eb
MarrMika <slobodjanmarija1@gmail.com>
2020-06-25 23:50:35 +0300

    Fixed an interceptor filter logic that is used to filter the tenant id existence.

--- src/main/java/org/speech4j/tenantservice/config/multitenancy/TenantInterceptor.java
+++ src/main/java/org/speech4j/tenantservice/config/multitenancy/TenantInterceptor.java
@@ -1,8 +1,7 @@
 package org.speech4j.tenantservice.config.multitenancy;
 
 import lombok.extern.slf4j.Slf4j;
-import org.speech4j.tenantservice.exception.TenantNotFoundException;
-import org.speech4j.tenantservice.repository.metadata.TenantRepository;
+import org.speech4j.tenantservice.service.TenantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -10,44 +9,33 @@ import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
-
 
 @Slf4j
 @Component
 public class TenantInterceptor implements WebFilter {
 
-    private TenantRepository tenantRepository;
+    private TenantService service;
 
     @Autowired
-    public TenantInterceptor(TenantRepository tenantRepository) {
-        this.tenantRepository = tenantRepository;
+    public TenantInterceptor(TenantService service) {
+        this.service = service;
     }
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        String schema = exchange.getRequest().getURI().getPath();
-        if (schema != null && !schema.isBlank() && schema.length() >= "/tenants/".length()) {
-            schema = schema.substring("/tenants/".length());
-            if (schema.contains("/")) {
-                schema = schema.substring(0, schema.indexOf("/"));
 
-                String finalSchema = schema;
-                getTenantIds().doOnSuccess(list -> {
-                    if (!list.contains(finalSchema)){
-                        Mono.error(new TenantNotFoundException("Tenant by a specified id: [" + finalSchema + "] not found!"));
-                    }
-                }).subscribe();
+        String url = exchange.getRequest().getURI().getPath();
+        if (url != null && !url.isBlank() && url.length() >= "/tenants/".length()) {
+            url = url.substring("/tenants/".length());
+            if (url.contains("/")) {
+                String schema = url.substring(0, url.indexOf("/"));
+                log.debug("INTERCEPTOR: Current schema was found: [{}]!", schema);
 
+                return service.getById(schema).flatMap(response-> chain.filter(exchange));
             }
         }
-
         return chain.filter(exchange);
-    }
 
-    private Mono<List<String>> getTenantIds() {
-        return tenantRepository.findAll()
-                .map(t -> t.getId())
-                .collectList();
+
     }
 }
